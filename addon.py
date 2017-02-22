e663a04b54cca8d5d4417e593ae4c4b9e8ba324c
ouija <mail@ouija.xyz>
2017-02-21 18:17:03 -0700

    changed back from Popen to call (need to wait); Added navigation back to home screen when executed.

--- addon.py
+++ addon.py
@@ -7,12 +7,13 @@ __addon__ = xbmcaddon.Addon()
 program = __addon__.getSetting('emulationstation_path')
 quitkodi = __addon__.getSetting('emulationstation_quitkodi')
 noexit = __addon__.getSetting('emulationstation_noexit')
+
+xbmc.executebuiltin( "XBMC.ActivateWindow(0)" )
+
 if quitkodi == 'true':    
 	if noexit == 'true':
 	    os.execv(program, [program, '--no-exit'])
 	elif noexit == 'false':
 	    os.execlp(program, program)
 elif quitkodi == 'false':	    
-		subprocess.Popen(program, shell=True)
-
-xbmc.executebuiltin( "XBMC.ActivateWindow(0)" )
+		subprocess.call(program, shell=True)
